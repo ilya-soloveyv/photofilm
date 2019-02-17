e289e7fc9277522ba70087e3bd906e2391b00468
ilya <ilya.soloveyv@gmail.com>
2019-02-18 02:06:31 +0300

    fix

--- app.js
+++ app.js
@@ -2,14 +2,70 @@ const express = require('express')
 const app = express()
 const pug = require('pug')
 app.set('view engine', 'pug');
+const config = require('config')
 require('dotenv').config()
 app.use(express.static('public'));
 app.locals.env = process.env;
 app.use(express.static('public'));
+const bodyParser = require('body-parser')
+app.use(bodyParser.urlencoded({ extended: true }))
+app.use(bodyParser.json())
+const moment = require('moment')
 
 app.use('/jquery', express.static(__dirname + '/node_modules/jquery/dist'))
 app.use('/bootstrap', express.static(__dirname + '/node_modules/bootstrap/dist'))
 app.use('/vue', express.static(__dirname + '/node_modules/vue/dist'))
+app.use('/axios', express.static(__dirname + '/node_modules/axios/dist'))
+
+var data = {}
+
+
+console.log(process.env.PORT)
+
+const Sequelize = require('sequelize')
+const connection = new Sequelize(process.env.DATABASE, process.env.USER, process.env.PASSWORD, {
+    "dialect": "mysql",
+    "operatorsAliases": false
+})
+
+var Entry = connection.define('entry', {
+    iObjectID: Sequelize.INTEGER,
+    dEntryDate: Sequelize.DATEONLY,
+    tEntryTimeFrom: Sequelize.TIME,
+    tEntryTimeTo: Sequelize.TIME,
+})
+
+// connection.sync().then(function(){
+    // Entry.create({
+    //     iObjectID: 1,
+    //     dEntryDate: '2019-02-18',
+    //     tEntryTimeFrom: '14:00:00',
+    //     tEntryTimeTo: '18:00:00'
+    // })
+// })
+// connection.sync().then(function(){
+    // Entry.findById(2).then(function (entry) {
+    //     console.log(entry.dataValues)
+    // })
+// })
+
+
+
+// sequelize.sync().then(() => Entry.findAll()).then(entries => {
+//     console.log(entries);
+// })
+
+
+// var models = require('./models');
+// var Empty = models.Empty;
+// var empty = Empty.build({ iObjectID: 1 });
+// const Entry = require('./models/entry.model')
+
+// Entry.findAll().then(entries => {
+//     console.log(entries)
+// })
+
+
 
 app.get('/', (req, res) => {
     res.sendStatus(503)
@@ -17,6 +73,82 @@ app.get('/', (req, res) => {
 
 app.get('/booking', (req, res) => {
     res.render('hello')
+})
+
+app.post('/get', async (req, res) => {
+    Entry.findAll({
+        where: {
+            iObjectID: req.body.iObjectID,
+            dEntryDate: req.body.dEntryDate
+        }        
+    }).then(function (entry) {
+        res.json(entry)
+    })
+    // data.body = req.body
+    // data.entry = await Entry.getEntrys({ dEntryDate: req.body.dEntryDate})
+    // data.entry = await Entry.getEntrys({ iObjectID: req.body.iObjectID, dEntryDate: req.body.dEntryDate })
+    
+})
+
+
+
+
+// var time = new Date(0, 0, 0, req.body.dEntryTimeFrom)
+// console.log(time)
+
+
+app.post('/set', async (req, res) => {
+    var from = new Date(req.body.dEntryDate + " " + req.body.tEntryTimeFrom)
+    var dEntryDate = moment(from).format('YYYY-MM-DD')
+    var tEntryTimeFrom = moment(from).format('HH:mm:ss')
+    var tEntryTimeTo = moment(from).add(req.body.orderHoutSelect, 'hours').format('HH:mm:ss')
+
+    Entry.create({
+        iObjectID: req.body.iObjectID,
+        dEntryDate: dEntryDate,
+        tEntryTimeFrom: tEntryTimeFrom,
+        tEntryTimeTo: tEntryTimeTo
+    }).then(function () {
+        res.json(true)
+    })
+
+    
+
+
+    // await Entry.setEntry([ req.body.iObjectID, dEntryDate, dEntryTimeFrom, dEntryTimeTo])
+
+
+    // const Entry = sequelize.define('entry', {
+    //     iEntryID: Sequelize.INTEGER,
+    //     iObjectID: Sequelize.INTEGER,
+    //     dEntryDate: Sequelize.DATE,
+    //     dEntryTimeFrom: Sequelize.TIME,
+    //     dEntryTimeTo: Sequelize.TIME
+    // })
+
+    // sequelize.sync().then(() => Entry2.create({
+    //     iObjectID: 1,
+    //     dEntryDate: '2019-02-17',
+    //     dEntryTimeFrom: '12:00:00',
+    //     dEntryTimeTo: '13:00:00',
+    // })).then(jane => {
+    //     console.log(jane.toJSON());
+    // });
+
+
+    // var d = new Date(0, 0, 0, 6, 0, 0)
+
+
+    // var d = new Date(0, 0, 0, 14, 0, 0)
+    // var jun = moment(d).tz('Europe/Moscow');
+    // console.log(jun)
+    
+    
+    // var dEntryTimeTo = new Date(req.body.dEntryTimeFrom)
+    //     dEntryTimeTo.setHour(dEntryTimeTo.getHour()+1)
+    // console.log(dEntryTimeTo)
+    // await Entry.getEntrys({ iObjectID: req.body.iObjectID, dEntryDate: req.body.dEntryDate })
+    
 })
 
 app.listen(process.env.PORT, () => {
--- public/js/app.js
+++ public/js/app.js
@@ -1,6 +1,25 @@
+(function() {
+    Date.prototype.toYMD = Date_toYMD;
+    function Date_toYMD() {
+        var year, month, day;
+        year = String(this.getFullYear());
+        month = String(this.getMonth() + 1);
+        if (month.length == 1) {
+            month = "0" + month;
+        }
+        day = String(this.getDate());
+        if (day.length == 1) {
+            day = "0" + day;
+        }
+        return year + "-" + month + "-" + day;
+    }
+})();
+
 var vm = new Vue({
     el: '#app',
     data: {
+        current_date: new Date(),
+        // current_date: new Date(2019, 0, 31),
         month: [
             [ 'Январь', 'Января' ],
             [ 'Февраль', 'Февраля' ],
@@ -15,128 +34,209 @@ var vm = new Vue({
             [ 'Ноябрь', 'Ноября' ],
             [ 'Декабрь', 'Декабря' ]
         ],
-        calendar: {
-            today: new Date(),
-            prev: {
-                year: null,
-                month: null,
-                day: null
-            },
-            current: {
-                date_string: null,
-                year: null,
-                month: null,
-                day: null,
-                full_date: null
-            },
-            next: {
-                year: null,
-                month: null,
-                day: null
-            }
-        },
         date: null,
-        // date: 15,
+        // date: new Date(2019, 1, 19),
         times: [
-            '00:00', '00:30', 
-            '01:00', '01:30', 
-            '02:00', '02:30', 
-            '03:00', '03:30', 
-            '04:00', '04:30', 
-            '05:00', '05:30', 
-            '06:00', '06:30', 
-            '07:00', '07:30', 
-            '08:00', '08:30', 
-            '09:00', '09:30', 
-            '10:00', '10:30', 
-            '11:00', '11:30', 
-            '12:00', '12:30', 
-            '13:00', '13:30', 
-            '14:00', '14:30', 
-            '15:00', '15:30', 
-            '16:00', '16:30', 
-            '17:00', '17:30', 
-            '18:00', '18:30', 
-            '19:00', '19:30', 
-            '20:00', '20:30', 
-            '21:00', '21:30', 
-            '22:00', '22:30', 
-            '23:00', '23:30', 
+            '00:00:00',
+            '01:00:00',
+            '02:00:00',
+            '03:00:00',
+            '04:00:00',
+            '05:00:00',
+            '06:00:00',
+            '07:00:00',
+            '08:00:00',
+            '09:00:00',
+            '10:00:00',
+            '11:00:00',
+            '12:00:00',
+            '13:00:00',
+            '14:00:00',
+            '15:00:00',
+            '16:00:00',
+            '17:00:00',
+            '18:00:00',
+            '19:00:00',
+            '20:00:00',
+            '21:00:00',
+            '22:00:00',
+            '23:00:00',
         ],
         time: null,
         entry: [
             {
                 
             }
-        ]
+        ],
+        objects: [
+            {
+                id: 1,
+                title: 'Помещение 1'
+            },
+            {
+                id: 2,
+                title: 'Помещение 2'
+            },
+            {
+                id: 3,
+                title: 'Помещение 3'
+            },
+            {
+                id: 4,
+                title: 'Помещение 4'
+            },
+            {
+                id: 5,
+                title: 'Помещение 5'
+            },
+        ],
+        current_object: 1,
+        orderHour: [
+            {
+                title: "Один час",
+                value: 1
+            },
+            {
+                title: "Два часа",
+                value: 2
+            },
+            {
+                title: "Три часа",
+                value: 3
+            },
+            {
+                title: "Четыре часа",
+                value: 4
+            },
+            {
+                title: "Пять часов",
+                value: 5
+            },
+        ],
+        orderHoutSelect: 1
+    },
+    created: function () {
+
     },
     mounted: function () {
-        this.calendar.current.date_string = new Date()
-        this.calendar.current.year = new Date().getFullYear()
-        this.calendar.current.month = new Date().getMonth()
-        this.calendar.current.day = new Date().getDate()
-        this.calendar.current.full_date = this.calendar.current.year + '-' + this.calendar.current.month + '-' + this.calendar.current.day
+
     },
     methods: {
         useDate: function (day) {
-            // alert(this.calendar.current.day)
-            // alert(day)
-            Vue.set(vm, 'date', day)
+            var useDate = new Date(this.current_date.getFullYear(), this.current_date.getMonth(), day)
+            Vue.set(vm, 'date', useDate)
+            Vue.set(vm, 'current_date', useDate)
+            this.getEntry()
         },
         setTime: function (time) {
             Vue.set(vm, 'time', time)
-            $('#exampleModal').modal()
+            // $('#exampleModal').modal()
+        },
+        prevMonth: function () {
+            let Y = this.current_date.getFullYear()
+            let M = this.current_date.getMonth()
+            let temp_date = new Date(Y, M, 1)
+                temp_date.setMonth(temp_date.getMonth()-1)
+            Vue.set(vm, 'current_date', temp_date)
+        },
+        nextMonth: function () {
+            let Y = this.current_date.getFullYear()
+            let M = this.current_date.getMonth()
+            let temp_date = new Date(Y, M, 1)
+                temp_date.setMonth(temp_date.getMonth()+1)
+            Vue.set(vm, 'current_date', temp_date)
+        },
+        prevDay: function () {
+            var newDate = new Date(this.date.getFullYear(), this.date.getMonth(), this.date.getDate())
+                newDate.setDate(newDate.getDate()-1)
+            Vue.set(vm, 'date', newDate)
+            Vue.set(vm, 'current_date', newDate)
+            this.getEntry()
+        },
+        nextDay: function () {
+            var newDate = new Date(this.date.getFullYear(), this.date.getMonth(), this.date.getDate())
+                newDate.setDate(newDate.getDate()+1)
+            Vue.set(vm, 'date', newDate)
+            Vue.set(vm, 'current_date', newDate)
+            this.getEntry()
+        },
+        clearDate: function () {
+            Vue.set(vm, 'date', null)
+            Vue.set(vm, 'time', null)
+        },
+        clearTime: function () {
+            Vue.set(vm, 'time', null)
+            this.getEntry()
+        },
+        getEntry: function () {
+            if (vm.date && vm.current_object) {
+                axios.post('/get', {
+                    dEntryDate: new Date(vm.date).toYMD(),
+                    iObjectID: vm.current_object
+                })
+                .then(function (response) {
+                    // console.log(response.data)
+                    Vue.set(vm, 'entry', response.data)
+                    Vue.nextTick(function () {
+                        $('.entry').each(function (i, v) {
+                            var from = $(this).attr('data-from')
+                            var to = $(this).attr('data-to')
+                            console.log(from, to)
+                            var top = $('.time[data-date="'+from+'"]').position().top
+                            var top2 = $('.time[data-date="'+to+'"]').position().top
+                            if (top2 === 0) { top2 = $('.times').height() }
+                            console.log(top, top2)
+                            // console.log(el)
+                            $(this).css({top: top}).height(top2-top)
+                        })
+                    })
+                })
+                .catch(function (error) {
+                    console.log(error)
+                })    
+            }
+        },
+        orderTime: function () {
+            axios.post('/set', {
+                iObjectID: this.current_object,
+                dEntryDate: this.dateString,
+                tEntryTimeFrom: this.time,
+                orderHoutSelect: this.orderHoutSelect
+            })
+            .then(function (response) {
+                vm.clearTime()
+            })
+            .catch(function (error) {
+                console.log(error)
+            })
         }
     },
     watch: {
-
+        current_object: function () {
+            this.getEntry()
+        }
     },
     computed: {
-        calendar_current_days: function () {
-            if (this.calendar.current.year !== null && this.calendar.current.month !== null) {
-                return new Date(this.calendar.current.year, this.calendar.current.month, 0).getDate()
-            } else {
-                return null
-            }
+        year: function () {
+            return this.current_date.getFullYear()
+        },
+        count_month_days: function () {
+            let Y = this.current_date.getFullYear()
+            console.log(Y)
+            let M = this.current_date.getMonth()
+            console.log(M)
+            
+            return 32 - new Date(Y, M, 32).getDate()
+        },
+        month_name: function () {
+            let M = this.current_date.getMonth()
+            return this.month[M][0]
+        },
+        currentDate: function () {
+            return this.date.getDate() + ' ' + this.month[this.date.getMonth()][0] + ' ' + this.date.getFullYear()
+        },
+        dateString: function () {
+            return new Date(vm.date).toYMD()
         }
-        // count_days_in_month: function () {
-        //     return this.daysInMonth()
-        // },
-        // prev_year: function () {
-        //     if (this.current_year && this.current_month) {
-        //         var temp = new Date(this.current_year,this.current_month,1)
-        //         temp.setMonth(this.current_month-1)
-        //         return temp.getFullYear()
-        //     } else {
-        //         return null
-        //     }
-        // },
-        // prev_month: function () {
-        //     if (this.current_year && this.current_month) {
-        //         var temp = new Date(this.current_year,this.current_month,1)
-        //         temp.setMonth(this.current_month-1)
-        //         return temp.getMonth()
-        //     } else {
-        //         return null
-        //     }
-        // },
-        // next_year: function () {
-        //     if (this.current_year && this.current_month) {
-        //         var temp = new Date(this.current_year,this.current_month,1)
-        //         temp.setMonth(this.current_month+1)
-        //         return temp.getFullYear()
-        //     } else {
-        //         return null
-        //     }
-        // },
-        // next_month: function () {
-        //     if (this.current_year && this.current_month) {
-        //         var temp = new Date(this.current_year,this.current_month,1)
-        //         temp.setMonth(this.current_month+1)
-        //         return temp.getMonth()
-        //     } else {
-        //         return null
-        //     }
-        // },
     }
 })
